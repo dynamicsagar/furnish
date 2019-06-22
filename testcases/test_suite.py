949a5437d033d11588ab74a798a70c40323e037a
dynamicsagar <dynamicsagar@gmail.com>
2019-06-22 15:55:14 +0530

    updated test suite with all test cases

--- testcases/test_suite.py
+++ testcases/test_suite.py
@@ -1,13 +1,25 @@
 import unittest
+from testcases.Dashboard.project_dashboard_ui_functionality.dashboard_ui_functionality import dashboardCases
+from testcases.filter_bucket.bulk_and_view_type import Bucket
+from testcases.furniture_budget.furniture_budget_testcases import Furniture
+from testcases.global_catalog.global_catalog_testcases import globalCatalog
+from testcases.login_logout.login_logout_testcases import LoginTests
+from testcases.add_remove_products.add_remove_products_testcses import AddRemove
 from testcases.catalog.catalog_testcases import Logcat
 
 # Get all tests from the test classes
 
+tc7 = unittest.TestLoader().loadTestsFromTestCase(Bucket)
+tc1 = unittest.TestLoader().loadTestsFromTestCase(dashboardCases)
+tc2 = unittest.TestLoader().loadTestsFromTestCase(LoginTests)
+tc4 = unittest.TestLoader().loadTestsFromTestCase(globalCatalog)
+tc5 = unittest.TestLoader().loadTestsFromTestCase(Furniture)
+tc6 = unittest.TestLoader().loadTestsFromTestCase(AddRemove)
 tc3 = unittest.TestLoader().loadTestsFromTestCase(Logcat)
 
 
 # Create a test suite combining all test classes
-smokeTest = unittest.TestSuite([tc3])
+smokeTest = unittest.TestSuite([tc3, tc1, tc2, tc7, tc4, tc5, tc6])
 
 
 unittest.TextTestRunner(verbosity=2).run(smokeTest)
